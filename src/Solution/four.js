14df9dd01a5f0d0231ad121f3695c78132d4a00d
Adrean1983 <adrean_@hotmail.com>
2019-02-28 22:59:12 -0500

    Assignment2 Max ReactCourse with Solutions

---
+++ src/Solution/four.js
@@ -0,0 +1,22 @@
+
+// // App
+// import Char from './Char/Char';
+
+
+// import React from 'react';
+
+// const char = () => {
+//     const style = {
+//         display: 'inline-block',
+//         padding: '16px',
+//         margin: '16px',
+//         border: '1px solid black',
+//         textAlign: 'center'
+//     }
+//     return (
+//         <div style={style}>
+
+//         </div>
+//     )
+// }
+// export default char;
